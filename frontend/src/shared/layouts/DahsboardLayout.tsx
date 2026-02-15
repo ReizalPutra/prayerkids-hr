20458e34dcc0e7054d531dd399179cfdb1a24dca
ReizalPutra <mohreizal27@gmail.com>
2026-02-16 00:35:28 +0700

    Fix typo in DashboardLayout component name and ensure proper export syntax

--- frontend/src/shared/layouts/DahsboardLayout.tsx
+++ frontend/src/shared/layouts/DahsboardLayout.tsx
@@ -2,12 +2,10 @@ import { Outlet } from 'react-router-dom';
 
 type Props = {}
 
-function DahsboardLayout({}: Props) {
-  return (
 function DashboardLayout({}: Props) {
   return (
     <div>DashboardLayout</div>
   )
 }
 
-export default DashboardLayout
+export default DashboardLayout;
